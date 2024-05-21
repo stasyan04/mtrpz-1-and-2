9e52f3d217c0dc0296a9833a26aeff1ca7a5d05c
Stanislav <handarulitst@gmail.com>
2024-05-22 00:30:26 +0300

    Add commander library and main file

---
+++ main.js
@@ -0,0 +1,23 @@
+import { Command } from "commander"
+import { readFile, writeFile } from "fs/promises"
+import replacer from "./replacer.js"
+
+const program = new Command()
+
+program
+    .argument("<path>", "Path to file to read")
+    .option("-o, --output <path>", "Path to file to write")
+    .parse(process.argv)
+
+
+const path = program.args[0]
+const output = program.output
+
+const text = await readFile(path, "utf-8")
+const newText = replacer(text)
+
+if (output) {
+    await writeFile(output, newText)
+} else {
+    console.log(newText)
+}
