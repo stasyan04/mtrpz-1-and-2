2fed4710b42ea3c0077f0ffcfc24476264b80b37
Stanislav <handarulitst@gmail.com>
2024-05-22 00:29:55 +0300

    replacer.js fix

--- replacer.js
+++ replacer.js
@@ -1,28 +1,28 @@
 'use strict'
 
 const openingTags = {
-    ['(?<=^|\s)\*\*(?=\S)']: '<b>',
-    ['(?<=^|\s)_(?=\S)']: '<i>',
-    ['(?<=^|\s)`(?=\S)']: '<tt>',
+    ['(?<=^|\\s)\\*\\*(?=\\S)']: '<b>',
+    ['(?<=^|\\s)_(?=\\S)']: '<i>',
+    ['(?<=^|\\s)`(?=\\S)']: '<tt>',
 }
 
 const closingTags = {
-   ['(?<=\S)\*\*(?=\s|$)']: '</b>',
-   ['(?<=\S)_(?=\s|$)']: '</i>',
-   ['(?<=\S)`(?=\s|$)']: '</tt>',
+   ['(?<=\\S)\\*\\*(?=\\s|$)']: '</b>',
+   ['(?<=\\S)_(?=\\s|$)']: '</i>',
+   ['(?<=\\S)`(?=\\s|$)']: '</tt>',
 }
 
-const allTags = [ ...Object.keys(openingTags), ...Object.keys(closingTags) ].join('|')
+const allTags = { ...openingTags, ...closingTags }
 
-const preformatted = '(?<=^|\n)```(.*)```(?=\n|$)'
-const preformattedTag = '<pre>$1</pre>'
+const preformatted = /(?<=^|\n|\r|\r\n)```([\s\S]+?)```(?=\n|\r|\r\n|$)/g
+const preformattedOpen = '<pre>'
+const preformattedClose = '</pre>'
 const tempPrefReplace = '$temp$'
 const preformattedPieces = []
 
 
-const paragraph = '(\r\n|\r|\n){2,}'
-const paragraphOpener = '<p>'
-const paragraphCloser = '</p>'
+const paragraph = /(\r\n|\r|\n){2,}/g
+const paragraphInHtml = '</p>\n<p>'
 
 export default (text) => {
     const newText = hidePreformatted(text)
@@ -35,7 +35,7 @@ export default (text) => {
 
 const hidePreformatted = (text) => {
     for (const piece of text.matchAll(preformatted)) {
-        preformattedPieces.push(piece[0])
+        preformattedPieces.push(piece[1])
         text = text.replace(piece[0], tempPrefReplace)
     }
 
@@ -44,47 +44,55 @@ const hidePreformatted = (text) => {
 
 const showPreformatted = (text) => {
     for (const piece of preformattedPieces) {
-        text = text.replace(tempPrefReplace, piece)
+        text = text.replace(tempPrefReplace, preformattedOpen + piece + preformattedClose)
     }
-    preformattedPieces = []
+    preformattedPieces.length = 0
 
     return text
 }
 
 const splitParagraphs = (text) => {
-    let closing = false
-
-    while (text.includes(paragraph)) {
-        text.replace(paragraph, closing ? paragraphCloser : paragraphOpener)
-        closing = !closing
-    }
+    text = '<p>' + text + '</p>'
+    text = text.replaceAll(paragraph, paragraphInHtml)
 
     return text
 }
 
 const findTags = (text) => {
-    const tags = text.matchAll(allTags)
-    let expected;
+    const listTags = []
+    for (const tag in allTags) {
+        const matches = text.matchAll(new RegExp(tag, 'g'))
+        
+        for (const match of matches) {
+            listTags.push([match.index, tag])
+        }
+    }
+    listTags.sort((a, b) => a[0] - b[0])
 
-    for (const tag of tags) {
-        if (expected && tag[0] !== expected) {
-            throw new Error(`Expected ${expected} but found ${tag[0]}`)
+    let expected = null
+    console.log(listTags)
+
+    for (const tag of listTags) {
+        if (expected && tag[1] !== expected) {
+            throw new Error(`Expected ${expected} but found ${tag[1]}`)
         }
 
         if (expected) {
             expected = null
-            text = text.replace(tag[0], closingTags[tag[0]])
+            text = text.replace(new RegExp(tag[1]), closingTags[tag[1]])
             continue
         }
 
-        const tagIndex = Object.keys(openingTags).indexOf(tag[0])
+        const expectedIndex = Object.keys(openingTags).indexOf(tag[1])
 
-        if (tagIndex === -1) {
-            throw new Error(`Unexpected closing tag ${tag[0]}`)
+        if (expectedIndex === -1) {
+            throw new Error(`Unexpected closing tag ${tag[1]}`)
         }
 
-        expected = Object.keys(closingTags)[tagIndex]
+        expected = Object.keys(closingTags)[expectedIndex]
 
-        text = text.replace(tag[0], openingTags[tag[0]])
+        text = text.replace(new RegExp(tag[1]), openingTags[tag[1]])
     }
+
+    return text
 }
