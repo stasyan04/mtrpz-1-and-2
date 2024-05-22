cb961c028af30e945cf4ed7c6a8aac9370c1a224
Stanislav <handarulitst@gmail.com>
2024-05-22 14:59:27 +0300

    Add ability to convert to console output

--- main.js
+++ main.js
@@ -6,20 +6,22 @@ const program = new Command()
 
 program
     .version("1.0.0")
-    .description("Replaces markdown tags with html tags")
+    .description("Replaces markdown tags with html tags or escape codes")
     .usage("<path> [options]")
 
 program
     .argument("<path>", "Path to file to read")
     .option("-o, --output <path>", "Path to file to write")
+    .option("-f, --format [format]", "Format to use", "html")
     .parse(process.argv)
 
 
 const path = program.args[0]
 const output = program.opts().output
+const format = program.opts().format
 
 const text = await readFile(path, "utf-8")
-const newText = replacer(text)
+const newText = replacer(text, format)
 
 if (output) {
     await writeFile(output, newText)
--- replacer.js
+++ replacer.js
@@ -1,22 +1,22 @@
 'use strict'
 
 const openingTags = {
-    ['(?<=^|\\s)\\*\\*(?=\\S)']: '<b>',
-    ['(?<=^|\\s)_(?=\\S)']: '<i>',
-    ['(?<=^|\\s)`(?=\\S)']: '<tt>',
+    ['(?<=^|\\s)\\*\\*(?=\\S)']: ['<b>', '\x1b[1m'],
+    ['(?<=^|\\s)_(?=\\S)']: ['<i>', '\x1b[3m'],
+    ['(?<=^|\\s)`(?=\\S)']: ['<tt>', '\x1b[7m'],
 }
 
 const closingTags = {
-   ['(?<=\\S)\\*\\*(?=\\s|$)']: '</b>',
-   ['(?<=\\S)_(?=\\s|$)']: '</i>',
-   ['(?<=\\S)`(?=\\s|$)']: '</tt>',
+   ['(?<=\\S)\\*\\*(?=\\s|$)']: ['</b>', '\x1b[0m'],
+   ['(?<=\\S)_(?=\\s|$)']: ['</i>', '\x1b[0m'],
+   ['(?<=\\S)`(?=\\s|$)']: ['</tt>', '\x1b[0m'],
 }
 
 const allTags = { ...openingTags, ...closingTags }
 
 const preformatted = /(?<=^|\n|\r|\r\n)```([\s\S]+?)```(?=\n|\r|\r\n|$)/g
-const preformattedOpen = '<pre>'
-const preformattedClose = '</pre>'
+const preformattedOpen = ['<pre>', '\x1b[7m']
+const preformattedClose = ['</pre>', '\x1b[0m']
 const tempPrefReplace = '$temp$'
 const preformattedPieces = []
 
@@ -24,11 +24,24 @@ const preformattedPieces = []
 const paragraph = /(\r\n|\r|\n){2,}/g
 const paragraphInHtml = '</p>\n<p>'
 
-export default (text) => {
+let mode = 0
+
+export default (text, format) => {
+    switch (format) {
+        case 'html':
+            mode = 0
+            break
+        case 'escape':
+            mode = 1
+            break
+        default:
+            throw new Error(`Unknown format: ${format}`)
+    }
+
     const newText = hidePreformatted(text)
 
     const withTags = findTags(newText)
-    const paragraphed = splitParagraphs(withTags)
+    const paragraphed = mode === 0 ? splitParagraphs(withTags) : withTags
 
     return showPreformatted(paragraphed)
 }
@@ -44,7 +57,7 @@ const hidePreformatted = (text) => {
 
 const showPreformatted = (text) => {
     for (const piece of preformattedPieces) {
-        text = text.replace(tempPrefReplace, preformattedOpen + piece + preformattedClose)
+        text = text.replace(tempPrefReplace, preformattedOpen[mode] + piece + preformattedClose[mode])
     }
     preformattedPieces.length = 0
 
@@ -78,7 +91,7 @@ const findTags = (text) => {
 
         if (expected) {
             expected = null
-            text = text.replace(new RegExp(tag[1]), closingTags[tag[1]])
+            text = text.replace(new RegExp(tag[1]), closingTags[tag[1]][mode])
             continue
         }
 
@@ -90,7 +103,7 @@ const findTags = (text) => {
 
         expected = Object.keys(closingTags)[expectedIndex]
 
-        text = text.replace(new RegExp(tag[1]), openingTags[tag[1]])
+        text = text.replace(new RegExp(tag[1]), openingTags[tag[1]][mode])
     }
 
     if (expected) {
